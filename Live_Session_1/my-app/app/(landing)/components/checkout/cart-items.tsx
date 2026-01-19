6133a5ade91f69d5a4c7c43a3ec18fbc5e1c7880
MhmmdAbdullah <adho77798@gmail.com>
2026-01-19 14:46:41 +0700

    add Live Session 3 Task

--- Live_Session_1/my-app/app/(landing)/components/checkout/cart-items.tsx
+++ Live_Session_1/my-app/app/(landing)/components/checkout/cart-items.tsx
@@ -1,17 +1,23 @@
 "use client";
 
-import { CartList } from "../ui/cart-popup";
 import Image from "next/image";
 import { FiCreditCard, FiTrash2 } from "react-icons/fi";
 import Button from "../ui/button";
 import PriceFormatter from "@/app/utils/price-formatter";
 import CardWithHeader from "../ui/card-with-header";
 import { useRouter } from "next/navigation";
+import { useCartStore } from "@/app/hooks/use-cart-store";
+import { getImageUrl } from "@/app/lib/api";
 
-const CartItems = () => {
+type TCartItems = {
+    handlePayment: () => void;
+}
+
+const CartItems = ({handlePayment}: TCartItems) => {
+    const {items, removeItem} = useCartStore();
     const {push} = useRouter();
 
-    const totalPrice = CartList.reduce((total, item) => total + item.price * item.qty, 0);
+    const totalPrice = items.reduce((total, item) => total + item.price * item.qty, 0);
 
     const Payment = () => {
         push("/payment")
@@ -20,15 +26,15 @@ const CartItems = () => {
     return (
         <CardWithHeader title="Cart Item">
             <div className="overflow-auto h-[300px]">
-                {CartList.map((item, index) => {
+                {items.length ? items.map((item) => {
                      return (
                         <div                             
-                            key={index}
+                            key={item._id}
                             className="border-b border-gray-200 p-4 flex gap-3"
                         >
                             <div className="bg-primary-light aspect-square w-16 flex justify-center items-center">
                                 <Image 
-                                    src={`/images/products/${item.imgUrl}`} 
+                                    src={getImageUrl(item.imageUrl)} 
                                     alt={item.name} 
                                     width={63} 
                                     height={63}
@@ -41,17 +47,29 @@ const CartItems = () => {
                                     <div className="text-primary">{PriceFormatter(item.price)}</div>
                                 </div>
                             </div>
-                            <Button size="small" variant="ghost" className="w-7 h-7 p-0! self-center ml-auto"><FiTrash2/></Button>
+                            <Button 
+                             size="small" 
+                             variant="ghost" 
+                             className="w-7 h-7 p-0! self-center ml-auto"
+                             onClick={() => removeItem(item._id)}
+                             >
+                                <FiTrash2/>
+                            </Button>
                         </div>       
                     );
-                })}    
+                }) : (
+                    <div className="text-center opacity-50 py-5">
+                        Your cart items is empty
+                    </div>
+                )
+                }    
             </div>
             <div className="p-4">
                 <div className="flex justify-between font-semibold">
                     <div className="text-sm">Total</div>
                     <div className="text-primary text-xs">{PriceFormatter(totalPrice)}</div>
                 </div>
-                <Button variant="dark" className="w-full mt-4" onClick={Payment}>
+                <Button variant="dark" className="w-full mt-4" onClick={handlePayment}>
                     <FiCreditCard size={22} /> Proceed to Payment
                 </Button>
             </div>       
